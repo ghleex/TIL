bfb1a3c87249f31f3ee19e7240133b6a5482ddd4
ghleex <ghleex@gmail.com>
2019-08-21 12:13:27 +0900

    2019-08-21 | :mag: django_orm_crud

---
+++ 03_django/01_django_orm_crud/articles/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):    # models.Model 에서 상속 받음
+    # id = models.AutoField(primary_key=True) # 자동으로 입력되도록 설정
+    # id(primary key)는 기본적으로 처음 생성 시 자동으로 만들어짐
+    title = models.CharField(max_length=10) # 클래스 변수: DB의 필드    
+    content = models.TextField()    # 클래스 변수: DB의 필드
+    created_at = models.DateTimeField(auto_now_add=True)    # 클래스 변수: DB의 필드
+    updated_at = models.DateTimeField(auto_now=True)
+
+    
+
+
